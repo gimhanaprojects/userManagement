264f1bb11feaec2d9f27c606018a95d53ceef720
GimhanaDissanayake <gimhanadev@gmail.com>
2022-09-21 21:55:41 +0530

    27. JWT Token provider - part 2

--- src/main/java/com/giimhana/userManagement/utility/JWTTokenProvider.java
+++ src/main/java/com/giimhana/userManagement/utility/JWTTokenProvider.java
@@ -1,14 +1,20 @@
 package com.giimhana.userManagement.utility;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.giimhana.userManagement.constant.SecurityConstant;
 import com.giimhana.userManagement.domain.UserPrincipal;
 
@@ -17,10 +23,35 @@ public class JWTTokenProvider {
     @Value("${jwt.secret}")
     private String secret;
 
-    public String generateJwtToken(UserPrincipal userPrincipal){
+    public String generateJwtToken(UserPrincipal userPrincipal) {
         String[] claims = getClaimFromUser(userPrincipal);
 
-        return JWT.create().withIssuer(SecurityConstant.GET_ARRAYS_LLC).withAudience(SecurityConstant.GET_ARRAYS_ADMINISTRATION).withIssuedAt(new Date()).withSubject(userPrincipal.getUsername()).withArrayClaim(SecurityConstant.AUTHORITIES, claims).withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstant.EXPIRATION_TIME)).sign(Algorithm.HMAC512(secret.getBytes()))
+        return JWT.create().withIssuer(SecurityConstant.GET_ARRAYS_LLC)
+                .withAudience(SecurityConstant.GET_ARRAYS_ADMINISTRATION).withIssuedAt(new Date())
+                .withSubject(userPrincipal.getUsername()).withArrayClaim(SecurityConstant.AUTHORITIES, claims)
+                .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstant.EXPIRATION_TIME))
+                .sign(Algorithm.HMAC512(secret.getBytes()));
+    }
+
+    public List<GrantedAuthority> getAuthorities(String token) {
+        String[] claims = getClaimsFromToken(token);
+        return Arrays.stream(claims).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    private String[] getClaimsFromToken(String token) {
+        JWTVerifier verifier = getJWTVerifier();
+        return verifier.verify(token).getClaim(SecurityConstant.AUTHORITIES).asArray(String.class);
+    }
+
+    private JWTVerifier getJWTVerifier() {
+        JWTVerifier verifier;
+        try {
+            Algorithm algorithm = Algorithm.HMAC512(secret);
+            verifier = JWT.require(algorithm).withIssuer(SecurityConstant.GET_ARRAYS_LLC).build();
+        } catch (JWTVerificationException exception) {
+            throw new JWTVerificationException(SecurityConstant.TOKEN_CANNOT_BE_VERIFIED);
+        }
+        return verifier;
     }
 
     private String[] getClaimFromUser(UserPrincipal user) {
