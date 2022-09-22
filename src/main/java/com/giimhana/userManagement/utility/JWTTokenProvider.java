df4270ae8e00fca954b04d53bb36faec7dc2ec94
GimhanaDissanayake <gimhanadev@gmail.com>
2022-09-22 23:49:06 +0530

    31. Authentication entry point

--- src/main/java/com/giimhana/userManagement/utility/JWTTokenProvider.java
+++ src/main/java/com/giimhana/userManagement/utility/JWTTokenProvider.java
@@ -4,12 +4,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
@@ -18,6 +23,7 @@ import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.giimhana.userManagement.constant.SecurityConstant;
 import com.giimhana.userManagement.domain.UserPrincipal;
 
+@Component
 public class JWTTokenProvider {
 
     @Value("${jwt.secret}")
@@ -36,6 +42,30 @@ public class JWTTokenProvider {
     public List<GrantedAuthority> getAuthorities(String token) {
         String[] claims = getClaimsFromToken(token);
         return Arrays.stream(claims).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    public Authentication getAuthentication(String username, List<GrantedAuthority> authorities,
+            HttpServletRequest request) {
+        UsernamePasswordAuthenticationToken userPasswordautToken = new UsernamePasswordAuthenticationToken(username,
+                null, authorities);
+        userPasswordautToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        return userPasswordautToken;
+    }
+
+    public boolean isTokenValid(String username, String token) {
+        JWTVerifier verifier = getJWTVerifier();
+
+        return org.apache.commons.lang3.StringUtils.isNotEmpty(username) && !isTokenExpired(verifier, token);
+    }
+
+    public String getSubject(String token) {
+        JWTVerifier verifier = getJWTVerifier();
+        return verifier.verify(token).getSubject();
+    }
+
+    private boolean isTokenExpired(JWTVerifier verifier, String token) {
+        Date expiration = verifier.verify(token).getExpiresAt();
+        return expiration.before(new Date());
     }
 
     private String[] getClaimsFromToken(String token) {
