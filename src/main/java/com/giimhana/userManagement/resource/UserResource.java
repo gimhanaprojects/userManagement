3254330b84dd7e801984f16a43d89c6064b1572c
GimhanaDissanayake <gimhanadev@gmail.com>
2022-10-02 23:19:45 +0530

    81. Get user temporrary profile image

--- src/main/java/com/giimhana/userManagement/resource/UserResource.java
+++ src/main/java/com/giimhana/userManagement/resource/UserResource.java
@@ -1,6 +1,9 @@
 package com.giimhana.userManagement.resource;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
@@ -144,6 +147,20 @@ public class UserResource extends ExceptionHandling {
             @PathVariable("fileName") String fileName) throws IOException {
         return Files
                 .readAllBytes(Paths.get(FileConstant.USER_FOLDER + username + FileConstant.FORWARD_SLASH + fileName));
+    }
+
+    @GetMapping(path = "/iamge/profile/{username}", produces = MimeTypeUtils.IMAGE_JPEG_VALUE)
+    public byte[] getTempProfileImage(@PathVariable("username") String username) throws IOException {
+        URL url = new URL(FileConstant.TEMP_PROFILE_IMAGE_BASE_URL + username);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try (InputStream inputStream = url.openStream()) {
+            int bytesRead;
+            byte[] chunk = new byte[1024];
+            while ((bytesRead = inputStream.read(chunk)) > 0) {
+                byteArrayOutputStream.write(chunk, 0, bytesRead);
+            }
+        }
+        return byteArrayOutputStream.toByteArray();
     }
 
     private ResponseEntity<HttpResponse> response(HttpStatus httpStatus, String message) {
